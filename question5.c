cb676f670e6cdf71c064149f34b1e012d68a9262
lokeshkatre <102735711+lokeshkatre@users.noreply.github.com>
2022-07-17 09:34:19 +0530

    Add files via upload

---
+++ question5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    for(int n=10; n>0;n--)
+    {
+        printf("%d ",2*n-1);
+    }
+    return 0;
+}
---
+++ question6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    for(int n=1;n<=10;n++)
+    {
+        printf("%d ",2*n);
+    }
+    return 0;
+}
